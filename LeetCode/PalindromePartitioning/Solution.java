4225fe20010a172b14eec9f9b6b9ad3ec5ecfa1c
xiaoyangmie <juyuanyang@gmail.com>
2013-08-24 21:56:01 -0400

    upload 2 empty problems

---
+++ LeetCode/PalindromePartitioning/Solution.java
@@ -0,0 +1,26 @@
+/*
+ * Palindrome Partitioning - Feb 28 - 7285 / 24780
+Given a string s, partition s such that every substring of the partition is a palindrome.
+
+Return all possible palindrome partitioning of s.
+
+For example, given s = "aab",
+Return
+
+  [
+    ["aa","b"],
+    ["a","a","b"]
+  ]
+ */
+
+package PalindromePartitioning;
+
+import java.util.ArrayList;
+
+public class Solution {
+	ArrayList<ArrayList<String>> res;
+	public ArrayList<ArrayList<String>> partition(String s) {
+		res = new ArrayList<ArrayList<String>>();
+		return res;
+	}
+}
