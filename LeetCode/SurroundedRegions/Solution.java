34bcce424c6149a928c41de6b11caac80e365a07
xiaoyangmie <juyuanyang@gmail.com>
2013-08-26 13:19:14 -0700

    initialize 2 problems

---
+++ LeetCode/SurroundedRegions/Solution.java
@@ -0,0 +1,30 @@
+/*
+ * Surrounded Regions - Feb 22 - 11049 / 41380
+
+Given a 2D board containing 'X' and 'O', capture all regions surrounded by 'X'.
+
+A region is captured by flipping all 'O's into 'X's in that surrounded region .
+
+For example,
+
+X X X X
+X O O X
+X X O X
+X O X X
+
+After running your function, the board should be:
+
+X X X X
+X X X X
+X X X X
+X O X X
+
+ */
+
+package SurroundedRegions;
+
+public class Solution {
+	public void solve(char[][] board) {
+		
+	}
+}
