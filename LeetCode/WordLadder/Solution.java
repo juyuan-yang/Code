0f54e238790f8f952c8f48330805f74267ea923c
xiaoyangmie <juyuanyang@gmail.com>
2013-08-28 02:33:37 -0400

    temp version for word ladder

--- LeetCode/WordLadder/Solution.java
+++ LeetCode/WordLadder/Solution.java
@@ -1,6 +1,7 @@
 /*
  * Word Ladder - Feb 11 - 10499 / 37276
-Given two words (start and end), and a dictionary, find the length of shortest transformation sequence from start to end, such that:
+Given two words (start and end), and a dictionary, 
+find the length of shortest transformation sequence from start to end, such that:
 
 Only one letter can be changed at a time
 Each intermediate word must exist in the dictionary
@@ -23,11 +24,58 @@ All words contain only lowercase alphabetic characters.
 
 package WordLadder;
 
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.HashSet;
+import java.util.List;
+import java.util.Map;
 
 public class Solution {
+	int res;
 	public int ladderLength(String start, String end, HashSet<String> dict) {
-		
-		return 0;
+		if(start == null || end == null || dict == null || dict.size() == 0) return 0;
+		Map<String, List<String>> map = generateMap(dict, start, end);
+		res = -1;
+		dfs(start, end, map, 1, new HashSet<String>());
+		return (res == -1) ? 0 : res;
+	}
+	
+	public void dfs(String currentWord, String end, Map<String, List<String>> map, int count, HashSet<String> visited) {
+		if(currentWord.equals(end)) {
+			if(res == -1 || count < res) res = count;
+			return;
+		}
+		if(res != -1 && count > res) return;
+		visited.add(currentWord); // bug2, forget visited Set, so get infinite loop
+		for(String word : map.get(currentWord)) {
+			if(!visited.contains(word)) dfs(word, end, map, count+1, visited);
+		}
+		visited.remove(currentWord);
+	}
+	
+	public Map<String, List<String>> generateMap(HashSet<String> dict, String start, String end) {
+		Map<String, List<String>> map = new HashMap<String, List<String>>();
+		map.put(start, generateList(start.toCharArray(), dict, end));
+		for(String word : dict) {
+			map.put(word, generateList(word.toCharArray(), dict, end));
+		}
+		return map;
+	}
+	
+	public List<String> generateList(char[] word, HashSet<String> dict, String end) {
+		List<String> list = new ArrayList<String>();
+		for(int i = 0; i < word.length; i++) {
+			char orig = word[i];
+			for(char ch = 'a'; ch <= 'z'; ch++) {
+				if(ch == orig) continue;		// bug1
+				word[i] = ch;
+				String newWord = new String(word);
+				if(dict.contains(newWord) || newWord.equals(end)) {
+					list.add(newWord);
+				}
+			}
+			word[i] = orig;
+		}
+		return list;
 	}
 }
