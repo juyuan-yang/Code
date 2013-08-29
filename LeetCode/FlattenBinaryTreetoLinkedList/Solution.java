b4c16087d398ecb5aaa79ce4cfc795041c2c8627
xiaoyangmie <juyuanyang@gmail.com>
2013-08-29 19:37:11 -0400

    initialize problems

---
+++ LeetCode/FlattenBinaryTreetoLinkedList/Solution.java
@@ -0,0 +1,38 @@
+/*
+ * Flatten Binary Tree to Linked List - Oct 14 '12 - 7105 / 21371
+Given a binary tree, flatten it to a linked list in-place.
+
+For example,
+Given
+
+         1
+        / \
+       2   5
+      / \   \
+     3   4   6
+The flattened tree should look like:
+   1
+    \
+     2
+      \
+       3
+        \
+         4
+          \
+           5
+            \
+             6
+Hints:
+If you notice carefully in the flattened tree, 
+each node's right child points to the next node of a pre-order traversal.
+ */
+
+package FlattenBinaryTreetoLinkedList;
+
+import Helper.TreeNode;
+
+public class Solution {
+	public void flatten(TreeNode root) {
+		
+	}
+}
---
+++ LeetCode/PathSum2/Solution.java
@@ -0,0 +1,34 @@
+/*
+ * Path Sum II - Oct 14 '12 - 6392 / 17591
+Given a binary tree and a sum, find all root-to-leaf paths where each path's sum equals the given sum.
+
+For example:
+Given the below binary tree and sum = 22,
+              5
+             / \
+            4   8
+           /   / \
+          11  13  4
+         /  \    / \
+        7    2  5   1
+return
+
+[
+   [5,4,11,2],
+   [5,8,4,5]
+]
+ */
+
+package PathSum2;
+
+import java.util.ArrayList;
+
+import Helper.TreeNode;
+
+public class Solution {
+	public ArrayList<ArrayList<Integer>> pathSum(TreeNode root, int sum) {
+		ArrayList<ArrayList<Integer>> res = new ArrayList<ArrayList<Integer>>();
+		
+		return res;
+	}
+}
