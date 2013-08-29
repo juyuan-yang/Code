687ff1fdf69b568e2191d3f8271e6c7552be949f
xiaoyangmie <juyuanyang@gmail.com>
2013-08-29 14:07:07 -0400

    initialize 2 problems

---
+++ LeetCode/PascalsTriangle/Solution.java
@@ -0,0 +1,27 @@
+/*
+ * Pascal's Triangle - Oct 28 '12 - 4857 / 10981
+Given numRows, generate the first numRows of Pascal's triangle.
+
+For example, given numRows = 5,
+Return
+
+[
+     [1],
+    [1,1],
+   [1,2,1],
+  [1,3,3,1],
+ [1,4,6,4,1]
+] 
+ */
+
+package PascalsTriangle;
+
+import java.util.ArrayList;
+
+public class Solution {
+	public ArrayList<ArrayList<Integer>> generate(int numRows) {
+		ArrayList<ArrayList<Integer>> res = new ArrayList<ArrayList<Integer>>();
+		
+		return res;
+	}
+}
---
+++ LeetCode/PascalsTriangle2/Solution.java
@@ -0,0 +1,22 @@
+/*
+ * Pascal's Triangle II - Oct 29 '12 - 5210 / 12287
+Given an index k, return the kth row of the Pascal's triangle.
+
+For example, given k = 3,
+Return [1,3,3,1].
+
+Note:
+Could you optimize your algorithm to use only O(k) extra space?
+ */
+
+package PascalsTriangle2;
+
+import java.util.ArrayList;
+
+public class Solution {
+	public ArrayList<Integer> getRow(int rowIndex) {
+		ArrayList<Integer> res = new ArrayList<Integer>();
+		
+		return res;
+	}
+}
