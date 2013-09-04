db69cd435cbaaf689c974ed168a8673f14c27d0f
xiaoyangmie <juyuanyang@gmail.com>
2013-09-04 17:34:26 -0400

    Longest Consecutive Sequence

--- LeetCode/LongestConsecutiveSequence/Solution.java
+++ LeetCode/LongestConsecutiveSequence/Solution.java
@@ -11,9 +11,34 @@ Your algorithm should run in O(n) complexity.
 
 package LongestConsecutiveSequence;
 
+import java.util.HashSet;
+import java.util.Set;
+
 public class Solution {
 	public int longestConsecutive(int[] num) {
+		if(num == null || num.length == 0) return 0;
 		
-		return 0;
+		int max = 0;
+		Set<Integer> set = new HashSet<Integer>();
+		for(int n : num) set.add(n);
+		for(int n : num) {
+			if(set.contains(n)) {
+				set.remove(n);
+				int res = 1 + search(n+1, set, true) + search(n-1, set, false);
+				if(res > max) max = res;
+			}
+		}
+		return max;
+	}
+	
+	public int search(int n, Set<Integer> set, boolean increase) {
+		int total = 0;
+		while(set.contains(n)) {
+			total++;
+			set.remove(n);
+			if(increase) n++;
+			else n--;
+		}
+		return total;
 	}
 }
