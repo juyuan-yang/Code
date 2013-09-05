93acdbf0416d9bd27ecad2d0ab311986774e2b4d
xiaoyangmie <juyuanyang@gmail.com>
2013-09-05 18:36:56 -0400

    ========= upload new problems

---
+++ LeetCode/ScrambleString/Solution.java
@@ -0,0 +1,51 @@
+/*
+ * Scramble String - Apr 30 '12 - 4644 / 14931
+Given a string s1, we may represent it as a binary tree 
+by partitioning it to two non-empty substrings recursively.
+
+Below is one possible representation of s1 = "great":
+
+    great
+   /    \
+  gr    eat
+ / \    /  \
+g   r  e   at
+           / \
+          a   t
+To scramble the string, we may choose any non-leaf node and swap its two children.
+
+For example, if we choose the node "gr" and swap its two children, 
+it produces a scrambled string "rgeat".
+
+    rgeat
+   /    \
+  rg    eat
+ / \    /  \
+r   g  e   at
+           / \
+          a   t
+We say that "rgeat" is a scrambled string of "great".
+
+Similarly, if we continue to swap the children of nodes "eat" and "at", 
+it produces a scrambled string "rgtae".
+
+    rgtae
+   /    \
+  rg    tae
+ / \    /  \
+r   g  ta  e
+       / \
+      t   a
+We say that "rgtae" is a scrambled string of "great".
+
+Given two strings s1 and s2 of the same length, determine if s2 is a scrambled string of s1.
+ */
+
+package ScrambleString;
+
+public class Solution {
+	public boolean isScramble(String s1, String s2) {
+		
+		return false;
+	}
+}
---
+++ LeetCode/Subsets2/Solution.java
@@ -0,0 +1,31 @@
+/*
+ * Subsets II - Jun 25 '12 - 4769 / 13419
+Given a collection of integers that might contain duplicates, S, return all possible subsets.
+
+Note:
+
+Elements in a subset must be in non-descending order.
+The solution set must not contain duplicate subsets.
+For example,
+If S = [1,2,2], a solution is:
+
+[
+  [2],
+  [1],
+  [1,2,2],
+  [2,2],
+  [1,2],
+  []
+]
+ */
+
+package Subsets2;
+
+import java.util.ArrayList;
+
+public class Solution {
+	public ArrayList<ArrayList<Integer>> subsetsWithDup(int[] num) {
+		
+		return null;
+	}
+}
