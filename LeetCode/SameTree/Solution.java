0f043bf0c4599909b94d15a521ee19347415ab11
xiaoyangmie <juyuanyang@gmail.com>
2013-09-05 15:27:15 -0400

    Update Solution.java
    Need to test in leetcode

--- LeetCode/SameTree/Solution.java
+++ LeetCode/SameTree/Solution.java
@@ -12,7 +12,9 @@ import Helper.TreeNode;
 
 public class Solution {
 	public boolean isSameTree(TreeNode p, TreeNode q) {
-		
-		return false;
+		if(p == null && q == null) return true;
+		else if(p != null && q != null) {
+			return (p.val == q.val) && isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
+		} else return false;
 	}
 }
