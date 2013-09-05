4e32da76fc29e70fdfa043c0d5cc26d147726c29
xiaoyangmie <juyuanyang@gmail.com>
2013-09-05 15:46:05 -0400

    Update Solution.java
    Need to test in leetcode

--- LeetCode/SymmetricTree/Solution.java
+++ LeetCode/SymmetricTree/Solution.java
@@ -28,7 +28,13 @@ import Helper.TreeNode;
 
 public class Solution {
 	public boolean isSymmetric(TreeNode root) {
-		
-		return false;
+		if(root == null) return true;
+		return isSymmetric(root.left, root.right);
 	}
+
+	public boolean isSymmetric(TreeNode p, TreeNode q) {
+		if(p == null && q == null) return true;
+		else if(p != null && q != null) {
+			return (p.val == q.val) && isSymmetric(p.left, q.right) && isSymmetric(p.right, q.left);
+		} else return false;
 }
