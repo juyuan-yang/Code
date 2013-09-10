d901e23974f9a37e809fe85bcae81b3999d72d6b
xiaoyangmie <juyuanyang@gmail.com>
2013-09-10 01:07:45 -0400

    temp version... not finished...

--- LeetCode/BinaryTreeInorderTraversal/Solution.java
+++ LeetCode/BinaryTreeInorderTraversal/Solution.java
@@ -18,12 +18,62 @@ Note: Recursive solution is trivial, could you do it iteratively?
 package BinaryTreeInorderTraversal;
 
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Stack;
 
 import Helper.TreeNode;
 
 public class Solution {
+	// iterative version is still a shit now :(
 	public ArrayList<Integer> inorderTraversal(TreeNode root) {
+		ArrayList<Integer> list = new ArrayList<Integer>();
+		if(root == null) return list;
 		
-		return null;
+		Stack<TreeNode> stack = new Stack<TreeNode>();
+		Map<TreeNode, Integer> map = new HashMap<TreeNode, Integer>();
+		stack.add(root);
+		map.put(root, 0);
+		
+		while(stack.size() > 0) {
+			TreeNode node = stack.pop();
+			addNode(node.right, map, stack, list);
+			addNode(node, map, stack, list);
+			addNode(node.left, map, stack, list);
+		}
+		return list;
+	}
+	
+	public void addNode(TreeNode node, Map<TreeNode, Integer> map,
+				Stack<TreeNode> stack, ArrayList<Integer> list) {
+		if(node != null) {
+			if(!map.containsKey(node)) {
+				map.put(node, 0);
+				stack.add(node);
+			} else {
+				int value = map.get(node);
+				if(value == 0){
+					map.put(node, 1);
+					stack.add(node);
+				} else if(value == 1){
+					list.add(node.val);
+					map.put(node, 2);
+				}
+			}
+		}
+	}
+	
+	// Recursive solution, AC on 1st try
+	public ArrayList<Integer> inorderTraversal_rec(TreeNode root) {
+		ArrayList<Integer> list = new ArrayList<Integer>();
+		visit(root, list);
+		return list;
+	}
+	
+	public void visit(TreeNode node, ArrayList<Integer> list) {
+		if(node == null) return;
+		visit(node.left, list);
+		list.add(node.val);
+		visit(node.right, list);
 	}
 }
--- LeetCode/WordLadder2/Solution.java
+++ LeetCode/WordLadder2/Solution.java
@@ -26,12 +26,61 @@ All words contain only lowercase alphabetic characters.
 package WordLadder2;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
 
+// temp version :(
 public class Solution {
 	public ArrayList<ArrayList<String>> findLadders(String start, String end, HashSet<String> dict) {
 		ArrayList<ArrayList<String>> res = new ArrayList<ArrayList<String>>();
+		if(start == null || end == null || dict.size() == 0) return res;
+		dict.add(start);
+		dict.add(end);
 		
+		Map<String, Boolean> visited = new HashMap<String, Boolean>();
+		for(String str : dict) visited.put(str, false);
+		Map<String, String> parent = new HashMap<String, String>();
+		Set<String> toVisit = new HashSet<String>();
+		boolean found = false;
+		
+		toVisit.add(start);
+		parent.put(start, "");
+		while(toVisit.size() > 0) {
+			Set<String> newSet = new HashSet<String>();
+			for(String str : toVisit) {
+				if(str.equals(end)) {
+					found = true;
+				}
+				visited.put(str, true);
+				newSet.addAll(findNextStrs(str, visited, parent, dict, end));
+			}
+			if(found) return res;
+			toVisit = newSet;
+		}
+		
+		return res;
+	}
+	
+	public ArrayList<String> findNextStrs(String word, Map<String, Boolean> visited, 
+				Map<String, String> parent, HashSet<String> dict, String end) {
+		ArrayList<String> res = new ArrayList<String>();
+		char[] str = word.toCharArray();
+		for(int i = 0; i < str.length; i++) {
+			char ch = str[i];
+			for(char newCh = 'a'; newCh <= 'z'; newCh++) {
+				if(newCh != ch) {
+					str[i] = newCh;
+					String temp = new String(str);
+					if(temp.equals(end) || (dict.contains(temp) && !visited.get(temp))) {
+						res.add(temp);
+						parent.put(temp, word);
+					}
+				}
+			}
+			str[i] = ch;
+		}
 		return res;
 	}
 }
