2280cf2178ee0dcbb944a2ceec123fbde5166711
xiaoyangmie <juyuanyang@gmail.com>
2013-09-15 16:21:43 -0400

    Update new problems, not in LeetCode

---
+++ Others/BinarySearchWithUnknownLength/Solution.java
@@ -0,0 +1,10 @@
+/*
+ * Do binary search without the length of array
+ * Use try catch to try accessing each index
+ */
+
+package BinarySearchWithUnknownLength;
+
+public class Solution {
+
+}
---
+++ Others/Max2PathInSquare/Solution.java
@@ -0,0 +1,10 @@
+/*
+ * Suppose you have an array of M * N, each cell has some amount if money in it
+ * Try to find two paths from (0,0) to (m,n), so that the money collected is max
+ */
+
+package Max2PathInSquare;
+
+public class Solution {
+
+}
