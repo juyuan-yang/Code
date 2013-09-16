45d2b7c01a111dd1f578a7d608fa09d8f47e5d05
xiaoyangmie <juyuanyang@gmail.com>
2013-09-16 01:26:34 -0400

    Unique Binary Search Trees 1 & Temp version for Word Ladder 2

--- LeetCode/UniqueBinarySearchTrees/Solution.java
+++ LeetCode/UniqueBinarySearchTrees/Solution.java
@@ -14,9 +14,17 @@ Given n = 3, there are a total of 5 unique BST's.
 
 package UniqueBinarySearchTrees;
 
+// AC on 2nd try :(
 public class Solution {
 	public int numTrees(int n) {
-		
-		return 0;
+		if(n <= 0) return 0;
+		int[] res = new int[n+1];
+		res[0] = res[1] = 1;
+		for(int i = 2; i <= n; i++) {
+			for(int mid = 1; mid <= i; mid++) {
+				res[i] += res[mid-1] * res[i-mid]; // bug: + => *
+			}
+		}
+		return res[n];
 	}
 }
--- LeetCode/WordLadder2/Solution.java
+++ LeetCode/WordLadder2/Solution.java
@@ -28,11 +28,26 @@ package WordLadder2;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.HashSet;
+import java.util.List;
 import java.util.Map;
 import java.util.Set;
 
-// temp version :(
+// Time Limit Exceed :(
 public class Solution {
+	public static void main(String[] args) {
+		Solution s = new Solution();
+		HashSet<String> set = new HashSet<String>();
+		set.add("a");
+		set.add("b");
+		set.add("c");
+		for(ArrayList<String> list : s.findLadders("a", "c", set)) {
+			for(String str : list) {
+				System.out.print(str + " ");
+			}
+			System.out.println();
+		}
+	}
+	
 	public ArrayList<ArrayList<String>> findLadders(String start, String end, HashSet<String> dict) {
 		ArrayList<ArrayList<String>> res = new ArrayList<ArrayList<String>>();
 		if(start == null || end == null || dict.size() == 0) return res;
@@ -41,30 +56,49 @@ public class Solution {
 		
 		Map<String, Boolean> visited = new HashMap<String, Boolean>();
 		for(String str : dict) visited.put(str, false);
-		Map<String, String> parent = new HashMap<String, String>();
+		Map<String, List<String>> parents = new HashMap<String, List<String>>();
 		Set<String> toVisit = new HashSet<String>();
-		boolean found = false;
+		int step = 1;
 		
 		toVisit.add(start);
-		parent.put(start, "");
+		parents.put(start, new ArrayList<String>());
 		while(toVisit.size() > 0) {
 			Set<String> newSet = new HashSet<String>();
 			for(String str : toVisit) {
 				if(str.equals(end)) {
-					found = true;
+					findAllRes(parents, res, step, 0, start, end, new String[step]);
+					return res;
 				}
 				visited.put(str, true);
-				newSet.addAll(findNextStrs(str, visited, parent, dict, end));
+				newSet.addAll(findNextStrs(str, visited, parents, dict, end));
 			}
-			if(found) return res;
 			toVisit = newSet;
+			step++;
 		}
-		
 		return res;
 	}
 	
+	public void findAllRes(Map<String, List<String>> parents, ArrayList<ArrayList<String>> res, 
+			int step, int curStep, String start, String curWord, String[] words) {
+		if(curStep == step) {
+			if(!curWord.equals(start)) return;
+			ArrayList<String> list = new ArrayList<String>();
+			for(int i = step-1; i >= 0; i--) list.add(words[i]);
+			res.add(list);
+			return;
+		} else if(curStep == 0) {
+			words[0] = curWord;
+			findAllRes(parents, res, step, curStep+1, start, curWord, words);
+			return;
+		}
+		for(String next : parents.get(curWord)) {
+			words[curStep] = next;
+			findAllRes(parents, res, step, curStep+1, start, next, words);
+		}
+	}
+	
 	public ArrayList<String> findNextStrs(String word, Map<String, Boolean> visited, 
-				Map<String, String> parent, HashSet<String> dict, String end) {
+				Map<String, List<String>> parents, HashSet<String> dict, String end) {
 		ArrayList<String> res = new ArrayList<String>();
 		char[] str = word.toCharArray();
 		for(int i = 0; i < str.length; i++) {
@@ -73,9 +107,16 @@ public class Solution {
 				if(newCh != ch) {
 					str[i] = newCh;
 					String temp = new String(str);
+					// as the end word could be reached from several words
 					if(temp.equals(end) || (dict.contains(temp) && !visited.get(temp))) {
 						res.add(temp);
-						parent.put(temp, word);
+						if(parents.get(temp) == null) {
+							ArrayList<String> list = new ArrayList<String>();
+							list.add(word);
+							parents.put(temp, list);
+						} else {
+							parents.get(temp).add(word);
+						}
 					}
 				}
 			}
