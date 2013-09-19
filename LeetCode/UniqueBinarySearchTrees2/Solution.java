0108b705c352845a4007da6a64bbe8ae3b7023f2
xiaoyangmie <juyuanyang@gmail.com>
2013-09-19 14:12:41 -0400

    Unique Binary Search Trees 2

--- LeetCode/UniqueBinarySearchTrees2/Solution.java
+++ LeetCode/UniqueBinarySearchTrees2/Solution.java
@@ -18,9 +18,36 @@ import java.util.ArrayList;
 
 import Helper.TreeNode;
 
+// AC on 3rd try :( need to be careful when start > end......
+// Anyway, I think current code is so short :)
 public class Solution {
 	public ArrayList<TreeNode> generateTrees(int n) {
-		
-		return null;
+		return generateTrees(1, n);
+	}
+	
+	public ArrayList<TreeNode> generateTrees(int start, int end) {
+		ArrayList<TreeNode> list = new ArrayList<TreeNode>();
+		if(start > end) {
+			list.add(null);
+			return list;
+		} else if(start == end) {
+			TreeNode node = new TreeNode(start);
+			list.add(node);
+			return list;
+		}
+		for(int i = start; i <= end; i++) {
+			ArrayList<TreeNode> lefts = generateTrees(start, i-1);
+			ArrayList<TreeNode> rights = generateTrees(i+1, end);
+			
+			for(TreeNode left : lefts) {
+				for(TreeNode right : rights) {
+					TreeNode node = new TreeNode(i);
+					node.left = left;
+					node.right = right;
+					list.add(node);
+				}
+			}
+		}
+		return list;
 	}
 }
