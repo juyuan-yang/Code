ef8f5877a55421e86d065cf9c17ddea92fad0046
xiaoyangmie <juyuanyang@gmail.com>
2013-09-25 13:35:25 -0400

    ============upload new problems

---
+++ LeetCode/CloneGraph/Solution.java
@@ -0,0 +1,36 @@
+/*
+ * Clone Graph - AC Rate: 29/175 - My Submissions
+Clone an undirected graph. Each node in the graph contains a label and a list of its neighbors.
+
+
+OJ's undirected graph serialization:
+Nodes are labeled from 0 to N - 1, where N is the total nodes in the graph.
+
+We use # as a separator for each node, and , as a separator for each neighbor of the node.
+As an example, consider the serialized graph {1,2#2#2}.
+
+The graph has a total of three nodes, and therefore contains three parts as separated by #.
+
+Connect node 0 to both nodes 1 and 2.
+Connect node 1 to node 2.
+Connect node 2 to node 2 (itself), thus forming a self-cycle.
+Visually, the graph looks like the following:
+
+       1
+      / \
+     /   \
+    0 --- 2
+         / \
+         \_/
+ */
+
+package CloneGraph;
+
+import Helper.UndirectedGraphNode;
+
+public class Solution {
+	public UndirectedGraphNode cloneGraph(UndirectedGraphNode node) {
+		
+		return null;
+	}
+}
