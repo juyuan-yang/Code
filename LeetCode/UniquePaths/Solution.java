382898b6c5733989ffa36e947070d57eb2bc44f7
xiaoyangmie <juyuanyang@gmail.com>
2013-09-29 14:03:54 -0400

    Unique Path 1&2

--- LeetCode/UniquePaths/Solution.java
+++ LeetCode/UniquePaths/Solution.java
@@ -1,8 +1,11 @@
 /*
- * Unique Paths AC Rate: 458/1415 My Submissions
-A robot is located at the top-left corner of a m x n grid (marked 'Start' in the diagram below).
+ * Unique Paths - AC Rate: 458/1415 - My Submissions
+A robot is located at the top-left corner of a m x n grid 
+(marked 'Start' in the diagram below).
 
-The robot can only move either down or right at any point in time. The robot is trying to reach the bottom-right corner of the grid (marked 'Finish' in the diagram below).
+The robot can only move either down or right at any point in time. 
+The robot is trying to reach the bottom-right corner of the grid 
+(marked 'Finish' in the diagram below).
 
 How many possible unique paths are there?
 
@@ -14,8 +17,21 @@ Note: m and n will be at most 100.
 
 package UniquePaths;
 
+// AC on 1st try :)
 public class Solution {
-	public int uniquePaths(int m, int n) {
-		return 0;
-	}
+    public int uniquePaths(int m, int n) {
+        if(m == 0 || n == 0) return 0;
+        int[][] res = new int[m][n];
+        
+        for(int i = 0; i < m; i++) res[i][0] = 1;
+        for(int j = 0; j < n; j++) res[0][j] = 1;
+        
+        for(int i = 1; i < m; i++){
+            for(int j = 1; j < n; j++){
+                res[i][j] = res[i-1][j] + res[i][j-1];
+            }
+        }
+        
+        return res[m-1][n-1];
+    }
 }
--- LeetCode/UniquePaths2/Solution.java
+++ LeetCode/UniquePaths2/Solution.java
@@ -21,9 +21,24 @@ Note: m and n will be at most 100.
 
 package UniquePaths2;
 
+// AC on 1st try :)
 public class Solution {
 	public int uniquePathsWithObstacles(int[][] obstacleGrid) {
+		if(obstacleGrid == null || obstacleGrid.length == 0 || obstacleGrid[0].length == 0)
+			return 0;
 		
-		return 0;
+		int m = obstacleGrid.length, n = obstacleGrid[0].length;
+		int[][] res = new int[m][n];
+		res[0][0] = (obstacleGrid[0][0] == 1) ? 0 : 1;
+				
+		for(int i = 1; i < m; i++) res[i][0] = (obstacleGrid[i][0] == 1) ? 0 : res[i-1][0];
+		for(int j = 1; j < n; j++) res[0][j] = (obstacleGrid[0][j] == 1) ? 0 : res[0][j-1];
+		
+		for(int i = 1; i < m; i++)
+			for(int j = 1; j < n; j++){
+				res[i][j] = (obstacleGrid[i][j] == 1) ? 0 : res[i-1][j] + res[i][j-1];
+			}
+		
+		return res[m-1][n-1];
 	}
 }
