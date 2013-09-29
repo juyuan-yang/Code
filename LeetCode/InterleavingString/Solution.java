c8c18aa706cb6c0f5a2eee1243a548ba94d6b57c
xiaoyangmie <juyuanyang@gmail.com>
2013-09-29 13:04:11 -0400

    Interleaving String

--- LeetCode/InterleavingString/Solution.java
+++ LeetCode/InterleavingString/Solution.java
@@ -13,9 +13,25 @@ When s3 = "aadbbbaccc", return false.
 
 package InterleavingString;
 
+// AC on 1st try :) to iterate using k is a good idea
 public class Solution {
 	public boolean isInterleave(String s1, String s2, String s3) {
+		if(s1 == null || s2 == null || s3 == null) return false;
+		if(s1.length() + s2.length() != s3.length()) return false;
+		if(s1.length() == 0) return s2.equals(s3);
+		if(s2.length() == 0) return s1.equals(s3);
 		
-		return false;
+		boolean[][] res = new boolean[s1.length() + 1][s2.length() + 1];
+		res[0][0] = true;
+		
+		for(int k = 1; k <= s3.length(); k++)
+			for(int i = 0; i <= s1.length(); i++) {
+				int j = k - i;
+				if(j < 0 || j > s2.length()) continue;
+				if(i > 0 && s3.charAt(k-1) == s1.charAt(i-1)) res[i][j] |= res[i-1][j];
+				if(j > 0 && s3.charAt(k-1) == s2.charAt(j-1)) res[i][j] |= res[i][j-1];
+			}
+		
+		return res[s1.length()][s2.length()];
 	}
 }
