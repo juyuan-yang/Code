694ff6d1512f00d4fdb6944e1e18368413c3961d
xiaoyangmie <juyuanyang@gmail.com>
2013-10-06 12:08:23 -0400

    Distinct Subsequences & Upload new problems

--- LeetCode/DistinctSubsequences/Solution.java
+++ LeetCode/DistinctSubsequences/Solution.java
@@ -15,8 +15,23 @@ Return 3.
 package DistinctSubsequences;
 
 public class Solution {
+	// AC on 2nd try? But I don't understand what I'm writing :(((
+	// why?
 	public int numDistinct(String S, String T) {
+		if(S == null || S.length() == 0) return 0;
+		if(T == null || T.length() == 0) return S.length();
 		
-		return 0;
+		int[][] res = new int[T.length()+1][S.length()+1];
+		for(int j = 0; j < S.length(); j++) res[0][j] = 1;
+		
+		for(int i = 0; i < T.length(); i++)
+			for(int j = 0; j < S.length(); j++){
+				res[i+1][j+1] = res[i+1][j];
+				if(T.charAt(i) == S.charAt(j)) {
+					res[i+1][j+1] += res[i][j];
+				}
+			}
+		
+		return res[T.length()][S.length()];
 	}
 }
---
+++ LeetCode/WordBreak2/Solution.java
@@ -0,0 +1,25 @@
+/*
+ * Word Break II - AC Rate: 23/214 - My Submissions
+Given a string s and a dictionary of words dict, 
+add spaces in s to construct a sentence where each word is a valid dictionary word.
+
+Return all such possible sentences.
+
+For example, given
+s = "catsanddog",
+dict = ["cat", "cats", "and", "sand", "dog"].
+
+A solution is ["cats and dog", "cat sand dog"].
+ */
+
+package WordBreak2;
+
+import java.util.ArrayList;
+import java.util.Set;
+
+public class Solution {
+	public ArrayList<String> wordBreak(String s, Set<String> dict) {
+		
+		return null;
+	}
+}
