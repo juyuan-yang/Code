a464364b9e71ca9f8f94e2c0eefb65ff94547f05
xiaoyangmie <juyuanyang@gmail.com>
2013-10-06 01:59:10 -0400

    Word Break & Copy List with Random Pointer

--- LeetCode/CopyListwithRandomPointer/Solution.java
+++ LeetCode/CopyListwithRandomPointer/Solution.java
@@ -8,11 +8,41 @@ Return a deep copy of the list.
 
 package CopyListwithRandomPointer;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import Helper.RandomListNode;
 
+// AC on 1st try :)
 public class Solution {
 	public RandomListNode copyRandomList(RandomListNode head) {
+		if(head == null) return null;
+		Map<RandomListNode, RandomListNode> map = new HashMap<RandomListNode, RandomListNode>();
+		RandomListNode newHead = new RandomListNode(head.label);
+		RandomListNode cur = head, newCur = newHead, newRandom = null, newNext = null;
+		map.put(head, newHead);
 		
-		return null;
+		while(cur != null) {
+			if(cur.random != null) {
+				if(map.containsKey(cur.random)) newRandom = map.get(cur.random);
+				else {
+					newRandom = new RandomListNode(cur.random.label);
+					map.put(cur.random, newRandom);
+				}
+				newCur.random = newRandom;
+			}
+			if(cur.next != null) {
+				if(map.containsKey(cur.next)) newNext = map.get(cur.next);
+				else {
+					newNext = new RandomListNode(cur.next.label);
+					map.put(cur.next, newNext);
+				}
+				newCur.next = newNext;
+			}
+			newCur = newCur.next;
+			cur = cur.next;
+		}
+		
+		return newHead;
 	}
 }
--- LeetCode/WordBreak/Solution.java
+++ LeetCode/WordBreak/Solution.java
@@ -12,10 +12,50 @@ Return true because "leetcode" can be segmented as "leet code".
 
 package WordBreak;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Set;
 
+// AC on 2nd try :(
 public class Solution {
 	public boolean wordBreak(String s, Set<String> dict) {
+		if(s == null || s.length() == 0 || dict == null || dict.size() == 0) return false;
+		
+		Map<Integer, ArrayList<Integer>> pos2nexts = new HashMap<Integer, ArrayList<Integer>>();
+		for(String word : dict) {
+			int begin = 0, temp;
+			while(true) {
+				temp = s.indexOf(word, begin);
+				if(temp == -1) break;
+				else {
+					if(pos2nexts.containsKey(temp)) pos2nexts.get(temp).add(word.length());
+					else {
+						ArrayList<Integer> list = new ArrayList<Integer>();
+						list.add(word.length());
+						pos2nexts.put(temp, list);
+					}
+				}
+				begin = temp + 1; // bug here, should use +1, as there may be duplicates, like aaaaaa
+			}
+		}
+		
+		boolean[] res = new boolean[s.length()];
+		res[0] = pos2nexts.containsKey(0);
+		int max = -1;
+		for(int i = 0; i < s.length(); i++) {
+			if(res[i]) {
+				if(pos2nexts.containsKey(i)) {
+					for(int step : pos2nexts.get(i)) {
+						if(i + step == s.length()) return true;
+						else if(i + step < s.length()) {
+							if(i + step > max) max = i + step;
+							res[i+step] = true;
+						}
+					}
+				}
+			} else if(i > max) return false;
+		}
 		
 		return false;
 	}
