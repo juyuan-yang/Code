d4b41e16841c1af649ac08aef1d191543885f863
xiaoyangmie <juyuanyang@gmail.com>
2013-10-07 22:53:15 -0400

    temp version... cannot submit to leetcode :(

--- LeetCode/FirstMissingPositive/Solution.java
+++ LeetCode/FirstMissingPositive/Solution.java
@@ -13,7 +13,19 @@ package FirstMissingPositive;
 
 public class Solution {
 	public int firstMissingPositive(int[] A) {
-		
-		return 0;
+		if(A == null || A.length == 0) return 0;
+		for(int i = 0; i < A.length; i++) {
+			while(A[i] != i + 1) {
+				if(A[i] > 0 && A[i] <= A.length && A[A[i]-1] != A[i]) {
+					int temp = A[i];
+					A[i] = A[A[i] - 1];
+					A[temp-1] = temp;
+				}
+			}
+		}
+		for(int i = 0; i < A.length; i++) {
+			if(A[i] != i + 1) return i+1;
+		}
+		return A.length+1;
 	}
 }
--- LeetCode/MaximalRectangle/Solution.java
+++ LeetCode/MaximalRectangle/Solution.java
@@ -1,13 +1,36 @@
 /*
  * Maximal Rectangle - Apr 24 '12 - 3792 / 13475
-Given a 2D binary matrix filled with 0's and 1's, find the largest rectangle containing all ones and return its area.
+Given a 2D binary matrix filled with 0's and 1's, 
+find the largest rectangle containing all ones and return its area.
  */
 
 package MaximalRectangle;
 
 public class Solution {
 	public int maximalRectangle(char[][] matrix) {
+		if(matrix == null || matrix.length == 0 || matrix[0].length == 0) return 0;
 		
-		return 0;
+		int m = matrix.length, n = matrix[0].length;
+		int[][] ones = new int[m][n];
+		
+		for(int i = 0; i < m; i++) {
+			ones[i][n-1] = (matrix[i][n-1] == '1') ? 1 : 0;
+			for(int j = n-2; j >= 0; j--) {
+				if(matrix[i][j] == '0') ones[i][j] = 0;
+				else ones[i][j] = ones[i][j+1] + 1;
+			}
+		}
+		
+		int res = 0;
+		for(int i = 0; i < m; i++)
+			for(int j = 0; j < n; j++){
+				int cur = i, minWidth = ones[i][j], max = ones[i][j];
+				while(cur < m && ones[i][cur] > 0) {
+					if(ones[i][cur] < minWidth) minWidth = ones[i][cur];
+					if((cur - i + 1) * minWidth > max) max = (cur - i + 1) * minWidth;
+				}
+				if(max > res) res = max;
+			}
+		return res;
 	}
 }
