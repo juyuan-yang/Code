3b4e1ca85247d0b55cd57aadf7574d1d591f6594
xiaoyangmie <juyuanyang@gmail.com>
2013-10-10 00:59:33 -0400

    Spiral Matrix 2

--- LeetCode/SpiralMatrix2/Solution.java
+++ LeetCode/SpiralMatrix2/Solution.java
@@ -15,9 +15,33 @@ You should return the following matrix:
 
 package SpiralMatrix2;
 
+// AC on 3rd try :(
 public class Solution {
+	private int[] Xs = new int[]{0, 1, 0, -1}, Ys = new int[]{1, 0, -1, 0};
+	
 	public int[][] generateMatrix(int n) {
-		
-		return null;
+		int[][] matrix = new int[n][n];
+		int cur = 1;
+		for(int i = 0; i < (n+1) / 2; i++) { // bug: should be n+1
+			cur = fillMatrix(matrix, i, n - 1 - i, cur);
+		}
+		return matrix;
+	}
+	
+	public int fillMatrix(int[][] matrix, int min, int max, int cur) {
+		if(min == max) {
+			matrix[min][min] = cur++;
+			return cur;
+		}
+		int i = min, j = min;
+		matrix[i][j] = cur++;
+		for(int k = 0; k < 4; ) {
+			i += Xs[k];
+			j += Ys[k];
+			if(i == min && j == min) break; // need to check before assign
+			matrix[i][j] = cur++;
+			if((i == min || i == max) && (j == min || j == max)) k++;
+		}
+		return cur;
 	}
 }
