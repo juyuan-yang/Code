3156a25bc1f09a3d7f9c1328d445d9793359ab28
xiaoyangmie <juyuanyang@gmail.com>
2013-10-13 01:02:22 -0400

    Multiply Strings & Anagrams

--- LeetCode/Anagrams/Solution.java
+++ LeetCode/Anagrams/Solution.java
@@ -8,11 +8,51 @@ Note: All inputs will be in lower-case.
 package Anagrams;
 
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
 
+// AC on 1st try :)
 public class Solution {
 	public ArrayList<String> anagrams(String[] strs) {
 		ArrayList<String> res = new ArrayList<String>();
-        for(String str : strs) res.add(str);
+		if(strs == null || strs.length == 0) return res;
+		
+		Map<String, String> sort2orig = new HashMap<String, String>();
+		Set<String> sortWords = new HashSet<String>();
+		
+        for(String str : strs) {
+        	String sort = sortWord(str);
+        	if(!sortWords.contains(sort)) {
+        		if(!sort2orig.containsKey(sort)) {
+        			sort2orig.put(sort, str);
+        		} else {
+        			res.add(str);
+        			sortWords.add(sort);
+        			res.add(sort2orig.get(sort));
+        			sort2orig.remove(sort);
+        		}
+        	} else {
+        		res.add(str);
+        	}
+        }
         return res;
+	}
+	
+	public String sortWord(String orig) {
+		int[] nums = new int[26];
+		for(int i = 0; i < orig.length(); i++) {
+			nums[orig.charAt(i) - 'a']++;
+		}
+		StringBuilder sb = new StringBuilder();
+		for(int i = 0; i < 26; i++) {
+			while(nums[i] > 0) {
+				sb.append('a' + i);
+				nums[i]--;
+			}
+		}
+		
+		return sb.toString();
 	}
 }
--- LeetCode/MultiplyStrings/Solution.java
+++ LeetCode/MultiplyStrings/Solution.java
@@ -7,9 +7,45 @@ Note: The numbers can be arbitrarily large and are non-negative.
 
 package MultiplyStrings;
 
+// AC after several tries :(... so many silly mistakes
 public class Solution {
 	public String multiply(String num1, String num2) {
+		if(num1 == null || num1.length() == 0 || num2 == null || num2.length() == 0) return "0";
 		
-		return null;
+		int[] n1 = numberConvertor(num1), n2 = numberConvertor(num2);
+		int[] bits = new int[n1.length + n2.length + 1];
+		
+		for(int i = 0; i < n1.length; i++)
+			for(int j = 0; j < n2.length; j++) {
+				int temp = i + j;
+				bits[temp] += n1[i] * n2[j];
+				while(bits[temp] > 9999) {
+					bits[temp+1] += bits[temp] / 10000;
+					bits[temp] %= 10000;
+					temp++;
+				}
+			}
+		
+		StringBuilder sb = new StringBuilder();
+		int index = bits.length - 1;
+		while(bits[index] == 0 && index > 0) index--;
+		sb.append(bits[index--]);
+		
+		for(int i = index; i >= 0; i--) {
+			if(bits[i] < 1000) sb.append("0");
+			if(bits[i] < 100) sb.append("0");
+			if(bits[i] < 10) sb.append("0");
+			sb.append(bits[i]);
+		}
+		
+		return sb.toString();
+	}
+	
+	public int[] numberConvertor(String num) {
+		int[] bits = new int[(num.length() + 3) / 4];
+		for(int i = num.length(), temp = 0; i > 0; i -= 4) {
+			bits[temp++] = Integer.parseInt(num.substring(((i-4 > 0) ? i-4 : 0), i));
+		}
+		return bits;
 	}
 }
