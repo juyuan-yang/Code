10ec61343edfa4b2fb4324964f5cf0699e182216
xiaoyangmie <juyuanyang@gmail.com>
2013-10-14 00:07:13 -0400

    Permutations II & Trapping Rain water

--- LeetCode/Permutations2/Solution.java
+++ LeetCode/Permutations2/Solution.java
@@ -10,10 +10,43 @@ For example,
 package Permutations2;
 
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
 
+// AC on 2nd try... The 1st is to verify, I cannot iterate the map, when I'm also changing the map
+// A little concern, as each recursion need to copy a set of int
 public class Solution {
 	public ArrayList<ArrayList<Integer>> permuteUnique(int[] num) {
-		
-		return null;
+		ArrayList<ArrayList<Integer>> res = new ArrayList<ArrayList<Integer>>();
+		if(num == null || num.length == 0) return res;
+		Map<Integer, Integer> num2count = new HashMap<Integer, Integer>();
+		for(int temp : num) {
+			if(num2count.containsKey(temp)) num2count.put(temp, num2count.get(temp)+1);
+			else num2count.put(temp, 1);
+		}
+		tryNext(res, num2count, new int[num.length], 0);
+		return res;
+	}
+	
+	public void tryNext(ArrayList<ArrayList<Integer>> res, Map<Integer, Integer> num2count,
+			int[] has, int cur) {
+		if(cur == has.length) {
+			ArrayList<Integer> list = new ArrayList<Integer>();
+			for(int temp : has) list.add(temp);
+			res.add(list);
+			return;
+		}
+		Set<Integer> current = new HashSet<Integer>();
+		current.addAll(num2count.keySet());
+		for(int temp : current) {
+			has[cur] = temp;
+			if(num2count.get(temp) == 1) num2count.remove(temp);
+			else num2count.put(temp, num2count.get(temp) - 1);
+			tryNext(res, num2count, has, cur+1);
+			if(num2count.containsKey(temp)) num2count.put(temp, num2count.get(temp) + 1);
+			else num2count.put(temp, 1);
+		}
 	}
 }
--- LeetCode/TrappingRainWater/Solution.java
+++ LeetCode/TrappingRainWater/Solution.java
@@ -1,19 +1,43 @@
 /*
  * Trapping Rain Water - AC Rate: 632/2344 My Submissions
-Given n non-negative integers representing an elevation map where the width of each bar is 1, compute how much water it is able to trap after raining.
+Given n non-negative integers representing an elevation map where the width of each bar is 1, 
+compute how much water it is able to trap after raining.
 
 For example, 
 Given [0,1,0,2,1,0,1,3,2,1,2,1], return 6.
 
 
-The above elevation map is represented by array [0,1,0,2,1,0,1,3,2,1,2,1]. In this case, 6 units of rain water (blue section) are being trapped. Thanks Marcos for contributing this image!
+The above elevation map is represented by array [0,1,0,2,1,0,1,3,2,1,2,1]. 
+In this case, 6 units of rain water (blue section) are being trapped. 
+Thanks Marcos for contributing this image!
  */
 
 package TrappingRainWater;
 
+import java.util.Stack;
+
+// AC after several tries :(
 public class Solution {
 	public int trap(int[] A) {
-		
-		return 0;
+		if(A == null || A.length == 0) return 0;
+		Stack<Integer> stack = new Stack<Integer>();
+		int res = 0, i = 0;
+		while(i < A.length) {
+			if(stack.isEmpty()) {
+				if(A[i] > 0) stack.add(i++);
+				else i++; // bug, forgot this
+			} else if(A[i] < A[stack.peek()]) {
+				stack.add(i++);
+			} else if(A[i] >= A[stack.peek()]) { // bug, need = here
+				int pre = stack.pop();
+				if(stack.isEmpty()) {
+					stack.add(i++);
+				} else {
+					int height = (Math.min(A[i], A[stack.peek()])) - A[pre];
+					res += height * (i - 1 - stack.peek());
+				}
+			}
+		}
+		return res;
 	}
 }
