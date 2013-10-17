85b9e6cd7430e12c4585caafa65340a3c92ad16d
xiaoyangmie <juyuanyang@gmail.com>
2013-10-17 17:32:27 -0400

    Text Justification & Rotate List

--- LeetCode/RotateList/Solution.java
+++ LeetCode/RotateList/Solution.java
@@ -11,9 +11,23 @@ package RotateList;
 
 import Helper.ListNode;
 
+// AC on 1st try :)
 public class Solution {
-	public ListNode rotateRight(ListNode head, int n) {
-		
-		return null;
-	}
+    public ListNode rotateRight(ListNode head, int n) {
+        if(head == null) return null;
+        int len = 1;
+        ListNode temp = head, tail = head;
+        for( ; tail.next != null; tail = tail.next) len++;
+        
+        n %= len;
+        if(n == 0) return head;
+        n = len - n;
+        
+        for(int l = 1; l < n; l++) temp = temp.next;
+        
+        tail.next = head;
+        head = temp.next;
+        temp.next = null;
+        return head;
+    }
 }
--- LeetCode/TextJustification/Solution.java
+++ LeetCode/TextJustification/Solution.java
@@ -1,10 +1,14 @@
 /*
- * Text Justification AC Rate: 119/960 My Submissions
-Given an array of words and a length L, format the text such that each line has exactly L characters and is fully (left and right) justified.
+ * Text Justification - AC Rate: 119/960 My Submissions
+Given an array of words and a length L, format the text such that each line 
+has exactly L characters and is fully (left and right) justified.
 
-You should pack your words in a greedy approach; that is, pack as many words as you can in each line. Pad extra spaces ' ' when necessary so that each line has exactly L characters.
+You should pack your words in a greedy approach; that is, pack as many words as you can 
+in each line. Pad extra spaces ' ' when necessary so that each line has exactly L characters.
 
-Extra spaces between words should be distributed as evenly as possible. If the number of spaces on a line do not divide evenly between words, the empty slots on the left will be assigned more spaces than the slots on the right.
+Extra spaces between words should be distributed as evenly as possible. 
+If the number of spaces on a line do not divide evenly between words, 
+the empty slots on the left will be assigned more spaces than the slots on the right.
 
 For the last line of text, it should be left justified and no extra space is inserted between words.
 
@@ -31,9 +35,56 @@ package TextJustification;
 
 import java.util.ArrayList;
 
+// AC on 2nd try... For 1st try, I missed that, last line no need of Justification
 public class Solution {
-	public ArrayList<String> fullJustify(String[] words, int L) {
-		
-		return null;
+	public static void main(String[] args) {
+		Solution s = new Solution();
+		s.fullJustify(new String[] {"What","must","be","shall","be."}, 12);
 	}
+	
+    public ArrayList<String> fullJustify(String[] words, int L) {
+        ArrayList<String> res = new ArrayList<String>();
+        for(int i = 0; i < words.length; i++) {
+            int length = words[i].length(), j = i;
+            while(length <= L) {
+                j++;
+                if(j == words.length) break;
+                length += 1 + words[j].length();
+            }
+            res.add(justify(words, i, j, L));
+            i = j - 1;
+        }
+        return res;
+    }
+    
+    public String justify(String[] words, int i, int j, int L) {
+    	StringBuilder sb = new StringBuilder();
+        sb.append(words[i]);
+        
+    	if(i+1 < j) {
+    		int len = 0;
+            for(int k = i; k < j; k++) len += words[k].length();
+            
+            int blanks = (L - len) / (j - i - 1);
+            int extra = L - len - blanks * (j - i - 1);
+            if(j == words.length) {
+            	blanks = 1;
+            	extra = 0;
+            }
+            
+            char[] blk = new char[blanks];
+            for(int k = 0; k < blanks; k++) blk[k] = ' ';
+            
+            for(int k = i + 1; k < j; k++) {
+            	sb.append(blk);
+            	if(extra > 0) {
+            		sb.append(' ');
+            		extra--;
+            	}
+            	sb.append(words[k]);
+            }
+    	} 
+        while(sb.length() < L) sb.append(' ');
+        return sb.toString();
+    }
 }
