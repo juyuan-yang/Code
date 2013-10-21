becc19b2c51323a51cc59d1b97d3dd6bfcc01888
xiaoyangmie <juyuanyang@gmail.com>
2013-10-21 01:03:52 -0400

    Longest Palindromic Substring & Longest Substring Without Repeating Characters

--- LeetCode/LongestPalindromicSubstring/Solution.java
+++ LeetCode/LongestPalindromicSubstring/Solution.java
@@ -7,8 +7,34 @@ and there exists one unique longest palindromic substring.
 
 package LongestPalindromicSubstring;
 
+// AC on 1st try :)
 public class Solution {
 	public String longestPalindrome(String s) {
-		return null;
+		if(s == null || s.length() == 0) return "";
+		String res = s.substring(0, 1);
+		
+		for(int i = 0; i < s.length() - 1; i++) {
+			int j = 1;
+			while(i - j >= 0 && i + j < s.length()) {
+				if(s.charAt(i-j) != s.charAt(i+j)) break;
+				else if(2*j + 1 > res.length()) {
+					res = s.substring(i-j, i+j+1);
+				}
+				j++;
+			}
+			if(s.charAt(i) == s.charAt(i+1)) {
+				if(res.length() < 2) res = s.substring(i, i+2);
+				j = 1;
+				while(i - j >= 0 && i + j + 1 < s.length()) {
+					if(s.charAt(i-j) != s.charAt(i+j+1)) break;
+					else if(2*j+2 > res.length()) {
+						res = s.substring(i-j, i+j+2);
+					}
+					j++;
+				}
+			}
+		}
+		
+		return res;
 	}
 }
--- LeetCode/LongestSubstringWithoutRepeatingCharacters/Solution.java
+++ LeetCode/LongestSubstringWithoutRepeatingCharacters/Solution.java
@@ -7,9 +7,31 @@ which the length is 3. For "bbbbb" the longest substring is "b", with the length
 
 package LongestSubstringWithoutRepeatingCharacters;
 
+import java.util.HashSet;
+import java.util.Set;
+
+// AC on 2nd try :( silly mistake
 public class Solution {
 	public int lengthOfLongestSubstring(String s) {
-		
-		return 0;
+        if(s == null || s.length() == 0) return 0;
+        
+        Set<Character> chs = new HashSet<Character>();
+        int start = 0, res = 1;
+        
+        for(int i = 0; i < s.length(); i++) {
+            char ch = s.charAt(i);
+            if(chs.contains(ch)) {
+                while(s.charAt(start) != ch && start != i) {
+                	chs.remove(s.charAt(start));
+                	start++;
+                }
+                start++;
+            } else {
+                chs.add(ch);
+                if(i - start + 1 > res) res = i - start + 1;
+            }
+        }
+        
+        return res;
 	}
 }
