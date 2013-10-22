5e1d1c627a4b80ccb44be493eaa85c3f37e04ba0
xiaoyangmie <juyuanyang@gmail.com>
2013-10-22 00:12:50 -0400

    Minimum Window Substring & Valid Number

--- LeetCode/MinimumWindowSubstring/Solution.java
+++ LeetCode/MinimumWindowSubstring/Solution.java
@@ -17,9 +17,55 @@ one unique minimum window in S.
 
 package MinimumWindowSubstring;
 
+import java.util.HashMap;
+import java.util.Map;
+
+// AC on 4th try :( Anyway it's O(n), rather than O(n*m)
+// n -> S.length(), m -> T.length()
 public class Solution {
-	public String minWindow(String S, String T) {
+    public String minWindow(String S, String T) {
+		Map<Character, Integer> ch2count = new HashMap<Character, Integer>();
+		for(int i = 0; i < T.length(); i++) {
+			char ch = T.charAt(i);
+			if(ch2count.containsKey(ch)) {
+				ch2count.put(ch, ch2count.get(ch) + 1);
+			} else ch2count.put(ch, 1);
+		}
 		
-		return null;
-	}
+		int nonZero = ch2count.size(), start = 0;
+		while(start < S.length() && !ch2count.containsKey(S.charAt(start))) start++; // bug, forgot to initialize
+		String res = "";
+		for(int i = 0; i < S.length(); i++) {
+			char ch = S.charAt(i);
+			if(ch2count.containsKey(ch)) {
+				int count = ch2count.get(ch);
+				ch2count.put(ch, count - 1);
+				if(count == 1) nonZero--;
+			}
+			while(start < i) { // bug, need to move start as much as possible
+				ch = S.charAt(start);
+				if(ch2count.containsKey(ch) && ch2count.get(ch) < 0) {
+					ch2count.put(ch, ch2count.get(ch) + 1);
+					start++;
+				} else if(!ch2count.containsKey(ch)) {
+				    start++;
+				} else break;
+			}
+			if(nonZero == 0) {
+				if(res.equals("") || i + 1 - start < res.length()) 
+					res = S.substring(start, i + 1);
+				while(start < i) {
+					ch = S.charAt(start);
+					if(ch2count.containsKey(ch)) {
+						if(nonZero != 0) break; // bug, need to move start as much as possible
+						int count = ch2count.get(ch);
+						ch2count.put(ch, count + 1);
+						if(count == 0) nonZero++;
+					}
+					start++;
+				}
+			}
+		}
+		return res;
+    }
 }
--- LeetCode/ValidNumber/Solution.java
+++ LeetCode/ValidNumber/Solution.java
@@ -1,5 +1,5 @@
 /*
- * Valid Number AC Rate: 205/2162 My Submissions
+ * Valid Number - AC Rate: 205/2162 My Submissions
 Validate if a given string is numeric.
 
 Some examples:
@@ -8,15 +8,53 @@ Some examples:
 "abc" => false
 "1 a" => false
 "2e10" => true
-Note: It is intended for the problem statement to be ambiguous. You should gather all requirements up front before implementing one.
+Note: It is intended for the problem statement to be ambiguous. 
+You should gather all requirements up front before implementing one.
  */
 
 package ValidNumber;
 
+// AC after several tries... see comments
 public class Solution {
-	public boolean isNumber(String s) {
-		
-		
-		return false;
-	}
+    public boolean isNumber(String s) {
+        if(s == null) return false;
+        s = s.trim();
+        if(s.length() == 0) return false;
+        if(s.contains("e")) {
+        	int index = s.indexOf('e');
+        	// ?e[int], after e, it must be int, rather than float
+        	return validNum(s.substring(0, index)) 
+        			&& validInt(s.substring(index + 1, s.length()), false, true);
+        } else {
+        	return validNum(s);
+        }
+    }
+    
+    public boolean validNum(String s) {
+        if(s.length() == 0) return false; // bug, need to check here
+    	if(s.charAt(0) == '+' || s.charAt(0) == '-') s = s.substring(1);
+    	if(s.contains(".")) {
+    		int index = s.indexOf('.');
+    		// s.length() > 1 to guarantee no "."
+    		return s.length() > 1 && validInt(s.substring(0, index), true, false) 
+    		        && validInt(s.substring(index + 1, s.length()), true, false);
+    	} else {
+    	    // s.length() > 0 to guarantee no "+" / "-"
+    		return s.length() > 0 && validInt(s, true, false);
+    	}
+    }
+    
+    // bug, validEmpty & hasSign needed... for numbers like, "6e", "1e+6"
+    public boolean validInt(String s, boolean validEmpty, boolean hasSign) {
+        if(s.length() == 0) return validEmpty; // bug, .1 and 3. is allowed...
+        if(hasSign) {
+            if(s.charAt(0) == '+' || s.charAt(0) == '-') s = s.substring(1);
+            if(s.length() == 0) return false; // bug, "6e+"
+        }
+        for(int i = 0; i < s.length(); i++) {
+        	char ch = s.charAt(i);
+        	if(!(ch >= '0' && ch <= '9')) return false; 
+        }
+        return true;
+    }
 }
