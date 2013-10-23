86ecf8feaefa28ed5b3d5aedb4a1f993100f7ac0
xiaoyangmie <juyuanyang@gmail.com>
2013-10-23 01:49:43 -0400

    Substring with Concatenation of All Words & Median of Two Sorted Arrays

--- LeetCode/MedianofTwoSortedArrays/Solution.java
+++ LeetCode/MedianofTwoSortedArrays/Solution.java
@@ -8,12 +8,13 @@ The overall run time complexity should be O(log (m+n)).
 
 package MedianofTwoSortedArrays;
 
+// AC after several tries :( shouldn't be that hard...
 public class Solution {
     public double findMedianSortedArrays(int A[], int B[]) {
     	if(A == null) A = new int[0];
         int l = A.length + B.length;
-        if(l % 2 == 0) 
-            return findKth(A, B, 0, A.length - 1, 0, B.length - 1, l / 2);
+        if(l % 2 == 1) 
+            return findKth(A, B, 0, A.length - 1, 0, B.length - 1, l / 2 + 1);
         else 
             return (findKth(A, B, 0, A.length - 1, 0, B.length - 1, l / 2) + findKth(A, B, 0, A.length - 1, 0, B.length - 1, l / 2 + 1)) / 2.0;
     }
@@ -22,13 +23,12 @@ public class Solution {
         if(as > ae) return B[bs + k - 1];
         if(bs > be) return A[as + k - 1];
         int am = (ae - as) / 2 + as, bm = (be - bs) / 2 + bs;
+        int l = am - as + bm - bs + 1;
         if(A[am] >= B[bm]) {
-            int l = am - as + bm - bs + 1;
-            if(k > l) return findKth(A, B, am, ae, bm+1, be, k - l);
+            if(k > l) return findKth(A, B, as, ae, bm+1, be, k - (bm - bs + 1)); // bug, k - ?
             else return findKth(A, B, as, am-1, bs, be, k);
         } else {
-            int l = am - as + bm - bs + 1;
-            if(k > l) return findKth(A, B, am+1, ae, bs, be, k - l);
+            if(k > l) return findKth(A, B, am+1, ae, bs, be, k - (am - as + 1));
             else return findKth(A, B, as, ae, bs, bm-1, k);
         }
     }
--- LeetCode/SubstringwithConcatenationofAllWords/Solution.java
+++ LeetCode/SubstringwithConcatenationofAllWords/Solution.java
@@ -15,10 +15,34 @@ You should return the indices: [0,9].
 package SubstringwithConcatenationofAllWords;
 
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
 
+// AC on 3rd try :(
 public class Solution {
 	public ArrayList<Integer> findSubstring(String S, String[] L) {
+		ArrayList<Integer> res = new ArrayList<Integer>();
+		Map<String, Integer> word2count = new HashMap<String, Integer>(); // should use map, as there may be duplicates...
+		for(String word : L) {
+			if(word2count.containsKey(word))
+				word2count.put(word, word2count.get(word) + 1);
+			else word2count.put(word, 1);
+		}
+		int wordLen = L[0].length();
 		
-		return null;
+		for(int i = 0; i <= S.length() - L.length * wordLen; i++) { // bug, should be <=
+			Map<String, Integer> temp = new HashMap<String, Integer>();
+			temp.putAll(word2count);
+			for(int j = i; j < i + L.length * wordLen; j += wordLen) {
+				String word = S.substring(j, j + wordLen);
+				if(temp.containsKey(word)) {
+					int count = temp.get(word);
+					if(count == 1) temp.remove(word);
+					else temp.put(word, count - 1);
+				} else break;
+			}
+			if(temp.isEmpty()) res.add(i);
+		}
+		return res;
 	}
 }
