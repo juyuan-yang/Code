d8c4da3ac20cc1419169bda68a07e12258c5ac85
xiaoyangmie <juyuanyang@gmail.com>
2013-11-03 23:12:08 -0500

    Simplified version for Search in Rotated Array 1&2

--- LeetCode/SearchinRotatedSortedArray/Solution.java
+++ LeetCode/SearchinRotatedSortedArray/Solution.java
@@ -12,43 +12,63 @@ You may assume no duplicate exists in the array.
 
 package SearchinRotatedSortedArray;
 
-// AC on 1st try :)
 public class Solution {
+	// Simplified version, AC on 2nd try
 	public int search(int[] A, int target) {
 		if(A == null || A.length == 0) return -1;
-		return rotatedBinarySearch(A, target, 0, A.length - 1);
-	}
-	
-	public int rotatedBinarySearch(int[] A, int target, int start, int end) {
-		if(start == end) return (target == A[start]) ? start : -1;
-		int mid = (start + end) / 2;
-		if(target == A[mid]) return mid;
 		
-		int res;
-		if(A[start] <= A[mid]) {
-			res = binarySearch(A, target, start, mid-1);
-		} else {
-			res = rotatedBinarySearch(A, target, start, mid-1);
-		}
-		if(res != -1) return res;
-		if(A[mid] <= A[end]) {
-			res = binarySearch(A, target, mid+1, end);
-		} else {
-			res = rotatedBinarySearch(A, target, mid+1, end);
-		}
-		return res;
-	}
-	
-	public int binarySearch(int[] A, int target, int start, int end) {
+		int start = 0, end = A.length - 1, mid;
 		while(start <= end) {
-			int mid = (start + end) / 2; // bug, put it before while :(
-			if(target == A[mid]) return mid;
-			else if(target > A[mid]) {
-				start = mid + 1;
-			} else {
-				end = mid - 1;
-			}
+		    mid = (end - start) / 2 + start;
+		    if(A[mid] == target) return mid;
+		    else if(A[start] <= A[mid]) { // should be <= rather than <, as start may == mid
+		        if(target < A[mid] && target >= A[start]) end = mid - 1;
+		        else start = mid + 1;
+		    } else {
+		        if(target > A[mid] && target <= A[end]) start = mid + 1;
+		        else end = mid - 1;
+		    }
 		}
+		
 		return -1;
 	}
+	
+	// AC on 1st try :)
+//	public int search(int[] A, int target) {
+//		if(A == null || A.length == 0) return -1;
+//		return rotatedBinarySearch(A, target, 0, A.length - 1);
+//	}
+//	
+//	public int rotatedBinarySearch(int[] A, int target, int start, int end) {
+//		if(start == end) return (target == A[start]) ? start : -1;
+//		int mid = (start + end) / 2;
+//		if(target == A[mid]) return mid;
+//		
+//		int res;
+//		if(A[start] <= A[mid]) {
+//			res = binarySearch(A, target, start, mid-1);
+//		} else {
+//			res = rotatedBinarySearch(A, target, start, mid-1);
+//		}
+//		if(res != -1) return res;
+//		if(A[mid] <= A[end]) {
+//			res = binarySearch(A, target, mid+1, end);
+//		} else {
+//			res = rotatedBinarySearch(A, target, mid+1, end);
+//		}
+//		return res;
+//	}
+//	
+//	public int binarySearch(int[] A, int target, int start, int end) {
+//		while(start <= end) {
+//			int mid = (start + end) / 2; // bug, put it before while :(
+//			if(target == A[mid]) return mid;
+//			else if(target > A[mid]) {
+//				start = mid + 1;
+//			} else {
+//				end = mid - 1;
+//			}
+//		}
+//		return -1;
+//	}
 }
--- LeetCode/SearchinRotatedSortedArray2/Solution.java
+++ LeetCode/SearchinRotatedSortedArray2/Solution.java
@@ -10,44 +10,71 @@ Write a function to determine if a given target is in the array.
 
 package SearchinRotatedSortedArray2;
 
-// AC after several tries :( Edge cases!
 public class Solution {
+	// AC on 3rd try :( A simplified version
 	public boolean search(int[] A, int target) {
 		if(A == null || A.length == 0) return false;
-		return rotatedBinarySearch(A, target, 0, A.length - 1);
-	}
-	
-	public boolean rotatedBinarySearch(int[] A, int target, int start, int end) {
-	    if(start > end) return false;
-		if(start == end) return target == A[start];
-		int mid = (start + end) / 2;
-		if(target == A[mid]) return true;
 		
-		boolean res;
-		if(A[start] < A[mid]) {
-			res = binarySearch(A, target, start, mid-1);
-		} else {
-			res = rotatedBinarySearch(A, target, start, mid-1);
-		}
-		if(res) return true;
-		if(A[mid] < A[end]) {
-			res = binarySearch(A, target, mid+1, end);
-		} else {
-			res = rotatedBinarySearch(A, target, mid+1, end);
-		}
-		return res;
+        return search(A, target, 0, A.length - 1);
 	}
 	
-	public boolean binarySearch(int[] A, int target, int start, int end) {
+	public boolean search(int[] A, int target, int start, int end) {
+	    int mid;
 		while(start <= end) {
-		    int mid = (start + end) / 2;
-			if(target == A[mid]) return true;
-			else if(target > A[mid]) {
-				start = mid + 1;
-			} else {
-				end = mid - 1;
-			}
+		    mid = (end - start) / 2 + start;
+		    if(A[mid] == target) return true;
+		    else if(A[start] < A[mid]) {
+		        if(target < A[mid] && target >= A[start]) end = mid - 1;
+		        else start = mid + 1;
+		    } else if(A[mid] < A[end]) {
+		        if(target > A[mid] && target <= A[end]) start = mid + 1;
+		        else end = mid - 1;
+		    } else { // bug here, start++ / end-- is not enough!
+		        if(search(A, target, start, mid - 1)) return true;
+		        else return search(A, target, mid + 1, end);
+		    }
 		}
+		
 		return false;
 	}
+	
+	// AC after several tries :( Edge cases!
+//	public boolean search(int[] A, int target) {
+//		if(A == null || A.length == 0) return false;
+//		return rotatedBinarySearch(A, target, 0, A.length - 1);
+//	}
+//	
+//	public boolean rotatedBinarySearch(int[] A, int target, int start, int end) {
+//	    if(start > end) return false;
+//		if(start == end) return target == A[start];
+//		int mid = (start + end) / 2;
+//		if(target == A[mid]) return true;
+//		
+//		boolean res;
+//		if(A[start] < A[mid]) {
+//			res = binarySearch(A, target, start, mid-1);
+//		} else {
+//			res = rotatedBinarySearch(A, target, start, mid-1);
+//		}
+//		if(res) return true;
+//		if(A[mid] < A[end]) {
+//			res = binarySearch(A, target, mid+1, end);
+//		} else {
+//			res = rotatedBinarySearch(A, target, mid+1, end);
+//		}
+//		return res;
+//	}
+//	
+//	public boolean binarySearch(int[] A, int target, int start, int end) {
+//		while(start <= end) {
+//		    int mid = (start + end) / 2;
+//			if(target == A[mid]) return true;
+//			else if(target > A[mid]) {
+//				start = mid + 1;
+//			} else {
+//				end = mid - 1;
+//			}
+//		}
+//		return false;
+//	}
 }
