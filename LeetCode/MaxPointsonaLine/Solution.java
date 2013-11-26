a5cba076d835e608fe332a35b1208ed6617c723c
xiaoyangmie <juyuanyang@gmail.com>
2013-11-26 18:04:18 -0500

    =================Upload new problems

---
+++ LeetCode/MaxPointsonaLine/Solution.java
@@ -0,0 +1,15 @@
+/*
+ * Max Points on a Line - Total Accepted: 571 Total Submissions: 7045 My Submissions
+Given n points on a 2D plane, find the maximum number of points that lie on the same straight line.
+ */
+
+package MaxPointsonaLine;
+
+import Helper.Point;
+
+public class Solution {
+	public int maxPoints(Point[] points) {
+		
+		return 0;
+	}
+}
---
+++ LeetCode/SortList/Solution.java
@@ -0,0 +1,15 @@
+/*
+ * Sort List - Total Accepted: 611 Total Submissions: 3391 My Submissions
+Sort a linked list in O(n log n) time using constant space complexity.
+ */
+
+package SortList;
+
+import Helper.ListNode;
+
+public class Solution {
+	public ListNode sortList(ListNode head) {
+		
+		return null;
+	}
+}
